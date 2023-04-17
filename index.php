2c5dc7b69edb91728bf6dc9e0b7fa4f20e820a1c
Mikel3010 <mikelgremi30@gmail.com>
2023-04-17 21:26:57 +0200

    creazione prodotti

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'product.php';
+
+$dogProduct= [
+    new DogProduct('Cuccia per cani', 100,'https://www.google.com/search?q=foto+cuccia+per+cani&rlz=1C1CHBF_itIT1039IT1039&sxsrf=APwXEdfRQEJLJqUQoIKC_yR0In0QK_WW3g:1681757099630&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj4ipa0ybH-AhXDSvEDHbGGBrsQ_AUoAXoECAEQAw&biw=1366&bih=695&dpr=1#imgrc=B5WZYXMsD3jjoM','Grande'),
+    new DogProduct('Guinzaglio per cani',30,'https://www.google.com/search?q=guinzaglio+cane&tbm=isch&ved=2ahUKEwi9yOC5ybH-AhUBp0wKHdSjBdAQ2-cCegQIABAA&oq=guinza&gs_lcp=CgNpbWcQARgCMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoECCMQJzoICAAQgAQQsQNQ1ghYsRFg3SBoAHAAeACAAXWIAcAEkgEDNi4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=t5M9ZP2QFIHOsgLUx5aADQ&bih=695&biw=1366&rlz=1C1CHBF_itIT1039IT1039#imgrc=kZwIdpnPxbXEkM','Medio'),
+];
+
+$catProduct =[
+    new CatProduct('Lettiera per gatti',50,'https://www.google.com/search?q=lettiera+gatto&tbm=isch&ved=2ahUKEwjdht_uybH-AhUWmycCHbVjAj8Q2-cCegQIABAA&oq=lettiera+&gs_lcp=CgNpbWcQARgAMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgQIABAeOgQIIxAnOgYIABAIEB46BggAEAcQHjoHCCMQ6gIQJ1D9B1jCKGDkMWgBcAB4AoABpgGIAbgNkgEEMTguM5gBAKABAaoBC2d3cy13aXotaW1nsAEKwAEB&sclient=img&ei=JpQ9ZN3bG5a2nsEPtceJ-AM&bih=695&biw=1366&rlz=1C1CHBF_itIT1039IT1039#imgrc=eW3mAx_1WtfbTM','Chiusa'),
+    new CatProduct('Giocattolo per gatti',10,'https://www.google.com/search?q=palla+per+gatto&tbm=isch&ved=2ahUKEwjM2pKTy7H-AhUL6qQKHevFDmMQ2-cCegQIABAA&oq=palla+per+gatto&gs_lcp=CgNpbWcQAzIGCAAQCBAeOgQIIxAnOgUIABCABDoGCAAQBxAeOggIABAIEAcQHlCME1i_IGDfJGgAcAB4AIABmAGIAfoHkgEDOS4ymAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=f5U9ZIyfDovUkwXri7uYBg&bih=695&biw=1366&rlz=1C1CHBF_itIT1039IT1039#imgrc=tz8ZOO8mgcjB6M','Palla'),
+];
+
+$fishProduct =[
+    new FishProduct('Acquario',200,'https://www.google.com/search?q=acquari+pesci&tbm=isch&ved=2ahUKEwjEnK6ry7H-AhWStKQKHSsjBuMQ2-cCegQIABAA&oq=acquari+pesci&gs_lcp=CgNpbWcQAzIECAAQHjIECAAQHjIECAAQHjIECAAQHjIECAAQHjIGCAAQCBAeMgYIABAIEB4yBggAEAgQHjIGCAAQCBAeMgYIABAIEB46BAgjECc6BQgAEIAEUIYPWMYfYOolaABwAHgAgAFliAHICJIBBDEzLjGYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=spU9ZMR7kumSBavGmJgO&bih=695&biw=1366&rlz=1C1CHBF_itIT1039IT1039#imgrc=cc0xBFUwiXqdgM','Dolce'),
+    new FishProduct('Cibo per pesci',5,'https://www.google.com/search?q=cibo+pesci&tbm=isch&ved=2ahUKEwihu6Huy7H-AhWNCewKHTkEAtAQ2-cCegQIABAA&oq=cibo+pesci&gs_lcp=CgNpbWcQAzIHCAAQgAQQEzIHCAAQgAQQEzIICAAQBxAeEBMyCAgAEAcQHhATMggIABAHEB4QEzIICAAQBxAeEBMyCAgAEAUQHhATMggIABAFEB4QEzIICAAQBRAeEBMyCAgAEAgQHhATOgQIIxAnOgQIABAeOgYIABAIEB46BggAEAcQHjoICAAQCBAHEB46CAgAEAUQBxAeUIgKWMgRYNoTaAFwAHgAgAFhiAHwA5IBATaYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=PpY9ZKGkE42TsAe5iIiADQ&bih=695&biw=1366&rlz=1C1CHBF_itIT1039IT1039#imgrc=WNcZqi5CcZ8foM','Dolce')
+];
+?>
