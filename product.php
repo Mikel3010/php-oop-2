687d5eaf4593af09b2f28d7a0b9053cc1c22d8ee
Mikel3010 <mikelgremi30@gmail.com>
2023-04-17 20:31:49 +0200

    classe padre

---
+++ product.php
@@ -0,0 +1,26 @@
+<?php
+
+class Product{
+    protected $name;
+    protected $price;
+    protected $image;
+
+    public function __construct($name,$price,$image)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->image = $image;
+    }
+
+    public function getName(){
+        return $this->name;
+    }
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function getImage(){
+        return $this->image;
+    }
+}
