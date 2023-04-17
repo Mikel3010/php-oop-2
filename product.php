57230467867c85f97601fc2e0697c7c6e4496529
Mikel3010 <mikelgremi30@gmail.com>
2023-04-17 20:38:12 +0200

    classi figlie

--- product.php
+++ product.php
@@ -24,3 +24,39 @@ class Product{
         return $this->image;
     }
 }
+
+class DogProduct extends Product{
+    protected $size;
+    public function __construct($name,$price,$image,$size)
+    {
+        parent::__construct($name,$price,$image);
+        $this->size = $size;
+    }
+    public function getSize(){
+        return $this->size;
+    }
+}
+
+class CatProduct extends Product{
+    protected $type;
+    public function __construct($name,$price,$image,$type)
+    {
+        parent::__construct($name,$price,$image);
+        $this->type = $type;
+    }
+    public function getType(){
+        return $this->type;
+    }
+}
+
+class FishProduct extends Product{
+    protected $waterType;
+    public function __construct($name,$price,$image,$waterType)
+    {
+        parent::__construct($name,$price,$image);
+        $this->waterType = $waterType;
+    }
+    public function getWaterType(){
+        return $this->waterType;
+    }
+}
